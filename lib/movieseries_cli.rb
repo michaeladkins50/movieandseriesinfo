fad73d25c4ff7b037b07ccacadb5d33315066b09
michaeladkins50 <mikeadkins1112@gmail.com>
2020-05-29 17:24:58 -0400

    bug fixes and windows termination

--- lib/movieseries_cli.rb
+++ lib/movieseries_cli.rb
@@ -3,5 +3,5 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 require_relative './movieseries_cli/cli.rb'
-require_relative './movieseries_cli/cli.rb'
-require_relative './movieseries_cli/cli.rb'
+require_relative './movieseries_cli/api.rb'
+require_relative './movieseries_cli/movieseries.rb'
